d6b4de8a34b2b1340b20de34cfcd897320ca2f6c
equintee <employes23@gmail.com>
2022-08-04 13:34:21 +0300

    EU

--- Assets/_Scripts/unitController.cs
+++ Assets/_Scripts/unitController.cs
@@ -1,22 +1,24 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class unitController : MonoBehaviour
 {
     private float moveSpeed;
     private Rigidbody rb;
-
+    private Animator animator;
     private Transform target;
+    public int hp;
+    public int damage;
     private void Start()
     {
         moveSpeed = FindObjectOfType<unitMatcher>().unitSpeed;
-        Debug.Log(moveSpeed);
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
-    Tweener lerper;
     public void setTarget(Transform target)
     {
         this.target = target;
@@ -26,14 +28,60 @@ public class unitController : MonoBehaviour
     private void Update()
     {
 
-        if (target != null && isMovingToTarget)
+        if (isMovingToTarget)
         {
             transform.DOLookAt(target.position, 0f);
             rb.position = Vector3.MoveTowards(rb.position, target.position, moveSpeed * Time.deltaTime);
 
             if (Vector3.Distance(rb.position, target.position) < 1f)
-                isMovingToTarget = false;
+            {
+                animateHit();
+            }
+                
         }
 
+    }
+
+    private async void animateHit()
+    {
+        isMovingToTarget = false;
+        rb.constraints = RigidbodyConstraints.FreezePosition;
+        animator.SetTrigger("attack");
+        await Task.Delay(System.TimeSpan.FromSeconds(1.5f));
+
+        target.GetComponent<unitController>().decrementHp(damage);
+        target.GetComponent<unitController>().animateGetHit();
+        isMovingToTarget = true;
+
+    }
+
+    public void decrementHp(int damage)
+    {
+        hp -= damage;
+    }
+
+    public async void animateGetHit()
+    {
+        isMovingToTarget = false;
+        if (hp > 0)
+        {
+            animator.SetTrigger("getHit");
+            
+        }
+        else
+            animateDeath();
+    }
+
+    public void animateDeath()
+    {
+        animator.SetTrigger("death");
+        this.enabled = false;
+        Invoke("destroyUnit()", 3f);
+    }
+
+    public void destroyUnit()
+    {
+        DOTween.Kill(rb);
+        Destroy(gameObject);
     }
 }
