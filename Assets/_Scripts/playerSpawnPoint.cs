821854584049677e95a610206a74dfb3a11ccf05
equintee <employes23@gmail.com>
2022-08-12 15:38:43 +0300

    EU

--- Assets/_Scripts/playerSpawnPoint.cs
+++ Assets/_Scripts/playerSpawnPoint.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class playerSpawnPoint : MonoBehaviour
 {
     public int spawnerLevel;
+    
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (!other.CompareTag("Player"))
+            return;
+
+        if(true)
+        {
+            
+        }
+        else
             other.gameObject.GetComponent<playerController>().spawnSkeletons(transform.position, spawnerLevel);
 
     }
