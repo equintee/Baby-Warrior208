b479a5efaa59e8957f278f5b82f4823de2409e7a
equintee <69682804+equintee@users.noreply.github.com>
2022-08-15 17:21:35 +0300

    EU

--- Assets/_Scripts/playerController.cs
+++ Assets/_Scripts/playerController.cs
@@ -108,11 +108,7 @@ public class playerController : MonoBehaviour
             //spawnedSkeleton.GetComponent<unitController>().moveToBridgeExit(playerBridgeExit);
         }
 
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-            animator.SetTrigger("run");
-
-
-        Invoke("setUpdate", 1f);
+        setUpdate();
     }
 
 
